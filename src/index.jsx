c8dece3e6ebd647c730f8a0e734589be52ea167a
Sean <53915643+sampelmedia@users.noreply.github.com>
2020-01-24 13:07:19 +0000

    test push

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<div>Hello World!</div>, global.document.getElementById('root'));
+render(<div>Surreal Estate</div>, global.document.getElementById('root'));

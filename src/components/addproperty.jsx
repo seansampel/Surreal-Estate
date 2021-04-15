4482245242c7d8db55a937fa232c8f06e870918a
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-04-15 14:44:54 +0100

    add select and option dropdown to add property

--- src/components/addproperty.jsx
+++ src/components/addproperty.jsx
@@ -5,23 +5,47 @@ const AddProperty = () => {
   const initialState = {
     fields: {
       title: '',
+      city: 'Manchester',
     },
   };
 
   // eslint-disable-next-line
   const [fields, setFields] = useState(initialState.fields);
 
-  const handleAddProperty = (event) => {
-    event.preventDefault();
-    console.log(fields);
+  // const handleAddProperty = (event) => {
+  //   event.preventDefault();
+  //   console.log(fields);
+  // };
+
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
   };
 
   return (
     <div className="Add-Property">
-      <form onSubmit={handleAddProperty}>
+      <form htmlFor="title">
+        Title
+        <input
+          id="title"
+          name="title"
+          value={fields.title}
+          onChange={handleFieldChange}
+        />
+        <label htmlFor="city">
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Sheffield">Sheffield</option>
+            <option value="Liverpool">Liverpool</option>
+          </select>
+        </label>
         <button type="submit">Add</button>
       </form>
-      <div><p>Hello</p></div>
     </div>
   );
 };
